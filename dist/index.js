d0ddedebda89d960cba7ddf13467db66fed79276
mohan cao <mohancao@yahoo.com.au>
2019-10-18 23:15:57 +1300

    Delete index.js

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./owoify-js.cjs.production.min.js')
-} else {
-  module.exports = require('./owoify-js.cjs.development.js')
-}
